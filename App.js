21d0d0b006cbdca35b3ba7650b445a84ea4dc622
Yeimer Molina <yeimer.molina@gmail.com>
2019-01-27 15:46:05 -0500

    Add navigation options on review screen

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
 import MapScreen from './screens/MapScreen';
 import DeckScreen from './screens/DeckScreen';
 import ReviewScreen from './screens/ReviewScreen'
+import SettingsScreen from './screens/SettingsScreen'
 
 const MainNavigator = createBottomTabNavigator({
   welcome: { screen: WelcomeScreen },
@@ -14,7 +15,12 @@ const MainNavigator = createBottomTabNavigator({
     screen: createBottomTabNavigator({
       map: { screen: MapScreen },
       deck: { screen: DeckScreen },
-      review: { screen: ReviewScreen }
+      review: { 
+        screen: createStackNavigator({
+          review: { screen: ReviewScreen },
+          settings: { screen: SettingsScreen }
+        }) 
+      }
     })
   }
 });
