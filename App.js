b40569ada8f5e2b36e063ed8b1d86362efd57a3a
Yeimer Molina <yeimer.molina@gmail.com>
2019-01-27 15:09:53 -0500

    Add Screens and main navigation component

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
+import ReviewScreen from './screens/ReviewScreen'
+
+const MainNavigator = createBottomTabNavigator({
+  welcome: { screen: WelcomeScreen },
+  auth: { screen: AuthScreen },
+  main: {
+    screen: createBottomTabNavigator({
+      map: { screen: MapScreen },
+      deck: { screen: DeckScreen },
+      review: { screen: ReviewScreen }
+    })
+  }
+});
+
+const AppNavigator = createAppContainer(MainNavigator)
 
 export default class App extends React.Component {
   render() {
+    
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <AppNavigator />
     );
   }
 }
@@ -14,7 +34,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ screens/SettingsScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class SettingsScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Settingd Screen</Text>
+      </View>
+    );
+  }
+}
+
+export default SettingsScreen;
