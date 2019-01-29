2fa98f7b3492b33729a03420f2a4374e386e01ef
Yeimer Molina <yeimer.molina@gmail.com>
2019-01-28 21:13:24 -0500

    Add redux, facebook login

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { createBottomTabNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
+import { Provider } from 'react-redux';
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
 import MapScreen from './screens/MapScreen';
 import DeckScreen from './screens/DeckScreen';
 import ReviewScreen from './screens/ReviewScreen'
 import SettingsScreen from './screens/SettingsScreen'
+import store from './store';
 
 const MainNavigator = createBottomTabNavigator({
   welcome: { screen: WelcomeScreen },
@@ -32,7 +34,10 @@ export default class App extends React.Component {
     
 
     return (
-      <AppNavigator />
+      <Provider store={store}>
+        <AppNavigator />
+      </Provider>
+      
     );
   }
 }
--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import {
+  facebookLogin
+} from '../actions';
 
 class AuthScreen extends Component {
+  componentDidMount() {
+    this.props.facebookLogin();
+  }
+
   render() {
     return (
       <View>
@@ -11,4 +19,4 @@ class AuthScreen extends Component {
   }
 }
 
-export default AuthScreen;
+export default connect(null, { facebookLogin })(AuthScreen);
