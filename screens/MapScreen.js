f97c513d0e80b6441bcec4f11ea38b9e3458189e
Yeimer Molina <yeimer.molina@gmail.com>
2019-02-02 18:15:02 -0500

    Add fetch job action creator

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
 import { MapView } from 'expo';
 
 class MapScreen extends Component {
   state = {
+    mapLoaded: false,
     region: {
       longitude: -122,
       latitude: 37,
@@ -12,12 +13,28 @@ class MapScreen extends Component {
     }
   }
 
+  componentDidMount() {
+    this.setState({ mapLoaded: true });
+  }
+
+  onRegionChangeComplete = (region) => {
+    this.setState({ region });
+  }
+
   render() {
+    if (!this.state.mapLoaded) {
+      return (
+        <View style={{flex: 1, justifyContent: 'center'}}>
+          <ActivityIndicator size='large'/>
+        </View>
+      )
+    }
     return (
       <View style={{ flex: 1 }}>
         <MapView 
           region={this.state.region}
           style={{ flex: 1 }}
+          onRegionChangeComplete={this.onRegionChangeComplete}
         />
       </View>
     );
