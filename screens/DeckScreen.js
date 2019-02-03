a7648a0e2b4a7288a31e3b4ff779a6529378490a
Yeimer Molina <yeimer.molina@gmail.com>
2019-02-03 09:54:16 -0500

    Add back button on renderNoMoreCards

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -38,10 +38,16 @@ class DeckScreen extends Component {
     );
   }
 
-  renderNoMoreCards() {
+  renderNoMoreCards = () => {
     return (
       <Card title="No More Jobs">
-
+        <Button 
+          title="Back To Map"
+          large
+          icon={{ name: 'my-location' }}
+          backgroundColor="#03A9F4"
+          onPress={() => this.props.navigation.navigate('map')}
+        />
       </Card>
     );
   }
